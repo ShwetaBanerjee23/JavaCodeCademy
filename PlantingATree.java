7c7d0d5b9c2bb2f43a6acf64f8bd492ca8014ada
ShwetaBanerjee23 <67150849+ShwetaBanerjee23@users.noreply.github.com>
2021-07-06 11:59:47 +0100

    Create PlantingATree.java

---
+++ PlantingATree.java
@@ -0,0 +1,14 @@
+public class PlantingATree {
+  public static void main(String[] args) {
+    // Here I will introduce myself and plant a tree to the screen
+    System.out.println("Hi, I am Shweta and I am learning to code in Java!");
+    System.out.println("I am going to plant a tree today!");
+    System.out.println("     *     ");
+    System.out.println("   *****   ");
+    System.out.println("  *******  ");
+    System.out.println(" ********* ");
+    System.out.println("***********");
+    System.out.println("    ***    ");
+    System.out.println("    ***    ");
+  }
+}
